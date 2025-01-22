0e073c1f296539bf4561d4538ed4b2efa9176258
angel9154 <angelgerenavazquez@outlook.com>
2025-01-22 10:24:16 -0500

    added a font roulette lol

--- app.ts
+++ app.ts
@@ -35,3 +35,44 @@ class Game { // encapsulate let counter and let multiplier for safer code
   $(document).ready(() => {
     new Game();
   });
+
+
+  $(document).ready(function() {
+    const fonts = [
+      'Arial, sans-serif',
+      'Courier New, Courier, monospace',
+      'Georgia, serif',
+      'Times New Roman, Times, serif',
+      'Verdana, Geneva, sans-serif',
+      'Trebuchet MS, sans-serif',
+      'Impact, Charcoal, sans-serif',
+      'Comic Sans MS, cursive',
+      'Lucida Console, Monaco, monospace',
+      'Palatino Linotype, Book Antiqua, Palatino, serif',
+      'Tahoma, Geneva, sans-serif',
+      'Lucida Sans Unicode, Lucida Grande, sans-serif',
+      'Arial Black, Gadget, sans-serif',
+      'Garamond, serif',
+      'Bookman Old Style, serif',
+      'Century Gothic, sans-serif',
+      'Copperplate, Copperplate Gothic Light, serif',
+      'Franklin Gothic Medium, Arial Narrow, sans-serif',
+      'Gill Sans, Gill Sans MT, Calibri, sans-serif',
+      'Helvetica, sans-serif'
+    ];
+  
+    let currentFontIndex = 0;
+  
+    $('#click-button').on('click', function() {
+      // Update the font of the #title element
+      $('#title').css('font-family', fonts[currentFontIndex]);
+  
+      // Move to the next font in the array
+      currentFontIndex++;
+  
+      // If we've reached the end of the array, loop back to the first font
+      if (currentFontIndex >= fonts.length) {
+        currentFontIndex = 0;
+      }
+    });
+  });
